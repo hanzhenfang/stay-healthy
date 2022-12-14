b3c78ec985d950de30364cd83f09b7fe133e3092
hanzhenfang <hanzhenfffang@gmail.com>
2022-12-14 09:12:39 +0800

    feat: 掘金《Promise》

--- src/demo/myPromise.ts
+++ src/demo/myPromise.ts
@@ -1,12 +1,23 @@
-function addUserAge(age: number, callBackFn: Function) {
-  setTimeout(() => {
-    const result = age + 1;
-    callBackFn(result);
-  }, 1000);
+export {};
+
+class MyPromise {
+  constructor(executor: Function) {
+    executor(this.resolve, this.reject);
+  }
+
+  resolve(value) {
+    console.log("我是手写的 resolve 传递的数据是", value);
+  }
+
+  reject() {}
 }
 
-const result = addUserAge(1, (result: number) => {
-  console.log("result", result);
+// MyPromise 的实例
+const data = new MyPromise((resolve, reject) => {
+  resolve("韩振方");
 });
 
-export {};
+// 原生 Promise
+new Promise((resolve, reject) => {
+  resolve("我是原生 resolve");
+});
--- src/view/Test/test1.vue
+++ src/view/Test/test1.vue
@@ -1,20 +1,62 @@
 <script setup lang="ts">
-setTimeout(() => {
-  console.log("1");
+import { promisify } from "util";
+
+class MyPromise {
+  #result: any;
+  #state: "pending" | "fulfilled" | "rejected";
+  constructor(executor: Function) {
+    this.#state = "pending";
+    executor(this.resolve.bind(this), this.reject.bind(this));
+  }
+
+  resolve(value) {
+    if (this.#state !== "pending") return;
+    this.#result = value;
+    this.#state = "fulfilled";
+  }
+
+  reject(value) {
+    if (this.#state !== "pending") return;
+    this.#result = value;
+    this.#state = "rejected";
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.#state === "fulfilled") {
+      onFulfilled(this.#result);
+    }
+    if (this.#state === "rejected") {
+      onRejected(this.#result);
+    }
+  }
+}
+
+// MyPromise 的实例
+const data = new MyPromise((resolve, reject) => {
+  resolve("1");
 });
 
-queueMicrotask(() => {
-  setTimeout(() => {
-    console.log("2");
-  });
+data.then(
+  (result) => {
+    console.log("result", result);
+  },
+  () => {}
+);
+
+console.log("我应该是第一执行的");
+
+//tips: 这是原生 Promise 类
+new Promise(() => {
+  console.log("我这里是同步代码");
 });
 
-new Promise((resolve) => {
-  resolve("3");
-}).then((res) => {
-  console.log(res);
-});
-
-console.log("4");
+// 原生 Promise
+// new Promise((resolve) => {
+//   resolve("1");
+//   resolve("2");
+//   resolve("3");
+// }).then((result) => {
+//   console.log("result", result);
+// });
 </script>
 <template></template>
